6dae3cb2a282829c4f39d176b0abb47f17f65779
mijikim <miji.kim926@gmail.com>
2014-06-23 16:43:25 -0600

    iterating array of hashes

--- lib/dogs.rb
+++ lib/dogs.rb
@@ -30,4 +30,104 @@ class Dogs
 
   # only edit below this line
 
+  def small_dogs
+
+  sdogs = []
+  @dogs.each do |dog|
+      if dog[:size] == :small
+        sdogs.push({name: dog[:name]})
+      end
+  end
+  sdogs
+  end
+
+  def huge_dog
+
+    hdogs = []
+    @dogs.each do |dog|
+      if dog[:size] == :huge
+        hdogs.push({name: dog[:name]})
+      end
+    end
+    hdogs
+  end
+
+  def large_dog_names
+  ldogs = []
+  @dogs.each do |dog|
+    if dog[:size] == :large
+      ldogs.push(dog[:name])
+    end
+  end
+  ldogs
+  end
+
+  def joes_large_dogs
+    jdogs = []
+    @dogs.each do |dog|
+      if dog[:size] == :large && dog[:owner][:name][:first] == "Joe"
+      jdogs.push(dog[:name])
+      end
+    end
+    jdogs
+  end
+
+  def sizes
+    sizes = []
+    @dogs.each do |dog|
+      sizes.push(dog[:size])
+    end
+    sizes.uniq
+
+  end
+
+  def owners
+    owners = []
+    @dogs.each do |dogs|
+      owners.push("#{dogs[:owner][:name][:first]} #{dogs[:owner][:name][:last]}")
+    end
+    owners.uniq
+  end
+
+  def average_owners
+    owners = []
+    @dogs.each do |dogs|
+      if dogs[:owner][:owner_quality] == AVERAGE
+      owners.push("#{dogs[:owner][:name][:first]} #{dogs[:owner][:name][:last]}")
+      end
+    end
+    owners.uniq
+  end
+  def to_s
+
+    jdogs = []
+    sdogs = []
+    adogs = []
+    @dogs.each do |dog|
+      if dog[:owner][:name][:first] == "Joe"
+        jdogs.push(dog[:name])
+      elsif dog[:owner][:name][:first] == "Sarah"
+        sdogs.push(dog[:name])
+      elsif dog[:owner][:name][:first] == "Andrew"
+        adogs.push(dog[:name])
+      end
+    end
+
+    "Joe owns: #{jdogs[0]}, #{jdogs[1]}, and #{jdogs[2]}\nSarah owns: #{sdogs[0]} and #{sdogs[1]}\nAndrew owns: #{adogs[0]} and #{adogs[1]}"
+
+  end
+
+  def find_by_owner(name)
+  alldogs = []
+  @dogs.each do |dog|
+
+    if dog[:owner][:name][:first] == name || dog[:owner][:name][:last] == name
+      alldogs.push(dog)
+    elsif
+    else
+      nil
+    end
+  end
+  alldogs
+  end
 end
